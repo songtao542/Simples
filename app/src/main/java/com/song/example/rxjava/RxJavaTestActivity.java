bf0f0c94ccda76cd74fc61fe0e5d90b51ef1cd9a
wangsongtao <songtao542@gmail.com>
2017-04-16 21:48:26 +0800

    Rxjava demo
    Signed-off-by: wangsongtao <songtao542@gmail.com>

--- app/src/main/java/com/song/example/rxjava/RxJavaTestActivity.java
+++ app/src/main/java/com/song/example/rxjava/RxJavaTestActivity.java
@@ -19,11 +19,15 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import dagger.Component;
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
+import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 
@@ -75,7 +79,57 @@ public class RxJavaTestActivity extends AppCompatActivity {
                         mText.setText("User:" + user);
                     }
                 });
+
+        Observable.fromArray(1, 2, 3, 4, 5)
+                .compose(new MyTransformer())
+                .map(new Function<Integer, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Integer integer) throws Exception {
+                        Log.d(LogTag.TAG, "map:" + integer);
+                        return integer * 10;
+                    }
+                })
+                .flatMap(new Function<Integer, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(@NonNull Integer integer) throws Exception {
+                        Log.d(LogTag.TAG, "flatMap:" + integer);
+                        return Observable.fromArray(integer + "a", integer + "b", integer + "c", integer + "d");
+                    }
+                })
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(@NonNull String s) throws Exception {
+                        Log.d(LogTag.TAG, "subscribe:" + s);
+                    }
+                });
     }
 
+    static class MyTransformer implements ObservableTransformer<Integer, Integer> {
+
+
+        @Override
+        public ObservableSource<Integer> apply(@NonNull Observable<Integer> upstream) {
+            return upstream
+                    .map(new Function<Integer, String>() {
+                        @Override
+                        public String apply(@NonNull Integer integer) throws Exception {
+                            Log.d(LogTag.TAG, "MyTransformer:map:1:" + integer);
+                            return "" + 100 + integer;
+                        }
+                    }).flatMap(new Function<String, ObservableSource<String>>() {
+                        @Override
+                        public ObservableSource<String> apply(@NonNull String s) throws Exception {
+                            Log.d(LogTag.TAG, "MyTransformer:flatMap:" + s);
+                            return Observable.fromArray(s + "1", s + "2", s + "3", s + "4", s + "5", s + "6");
+                        }
+                    }).map(new Function<String, Integer>() {
+                        @Override
+                        public Integer apply(@NonNull String s) throws Exception {
+                            Log.d(LogTag.TAG, "MyTransformer:map:2:" + s);
+                            return Integer.parseInt(s);
+                        }
+                    });
+        }
+    }
 
 }
